eccd7f5baeefa40896b7d9eac4e18a6c43deb0ec
dbrownell <danbrownell@comcast.net>
2020-01-21 09:41:41 -0500

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FlexgridComponent } from './flexgrid/flexgrid.component';
 import {AccordionModule} from 'primeng/accordion';
-//import {FlexGridModule} from 'primeflex/flexgrid';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, AccordionModule ],
+  imports:      [ BrowserModule, BrowserAnimationsModule, FormsModule, AccordionModule ],
   declarations: [ AppComponent, FlexgridComponent ],
   bootstrap:    [ AppComponent ]
 })
